0de6730a54622ad8f96fb3c48cedeb1d9bbba197
DEBASMI_BASU <83003226+debasmi@users.noreply.github.com>
2025-10-11 00:28:56 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,38 @@
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const texts = ["passionate web developer", "fast learner", "design enthusiast"];
+let currentTextIndex = 0;
+let currentCharIndex = 0;
+
+const dynamicTextElement = document.getElementById("dynamic-text");
+
+function typeText() {
+    if (currentCharIndex < texts[currentTextIndex].length) {
+        dynamicTextElement.textContent += texts[currentTextIndex][currentCharIndex];
+        currentCharIndex++;
+        setTimeout(typeText, 100); // Adjust typing speed
+    } else {
+        setTimeout(deleteText, 1000); // Pause before deleting
+    }
+}
+
+function deleteText() {
+    if (currentCharIndex > 0) {
+        dynamicTextElement.textContent = texts[currentTextIndex].substring(0, currentCharIndex - 1);
+        currentCharIndex--;
+        setTimeout(deleteText, 50); // Adjust deleting speed
+    } else {
+        currentTextIndex = (currentTextIndex + 1) % texts.length;
+        setTimeout(typeText, 500); // Pause before typing next text
+    }
+}
+
+typeText();
+
